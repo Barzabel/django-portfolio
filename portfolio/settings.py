ffd37d820314e7124b34883937ea89aae4891e00
Barzabel <kochenko1995@mail.ru>
2022-10-01 11:58:58 +0300

    fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-from .secret import key 
+from .secrets import key 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
